651f34ab29831700d47ad208dbc63d03b8da08c4
pro.boooooo <pro.boudjemline@gmail.com>
2023-10-19 18:15:36 +0200

    $

---
+++ src/Pages/Simflouz/Simflouz.jsx
@@ -0,0 +1,58 @@
+import CheatMesh from "../../Components/CheatMesh/CheatMesh"
+import Layout from './../../Layout/Layout';
+
+export default function Simflouz({ data }) {
+  return (
+    <Layout>
+      {Object.keys(data).length > 0 ? (
+        Object.keys(data).map((key) => (
+          <CheatMesh
+            title={data[key].title}
+            code={
+              data[key].args_i && data[key].args_r ? (
+                <div className="code-spec">
+                  <code>{data[key].code}</code>
+
+                  {data[key].args_i.map((value, index) => (
+                    <input type="text" placeholder={value} key={index} />
+                  ))}
+
+                  <select>
+                    {data[key].args_r.map((value, index) => (
+                      <option id={index} value={data[key].args_r_v[index]}>
+                        {value}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              ) : data[key].args_i || data[key].args_r ? (
+                <div className="code-spec">
+                  <code>{data[key].code}</code>
+
+                  {data[key].args_i ? (
+                    data[key].args_i.map((value, index) => (
+                      <input type="text" placeholder={value} key={index} />
+                    ))
+                  ) : (
+                    <select>
+                      {data[key].args_r.map((value, index) => (
+                        <option value={data[key].args_r_v[index]} id={index}>
+                          {value}
+                        </option>
+                      ))}
+                    </select>
+                  )}
+                </div>
+              ) : (
+                <code>{data[key].code}</code>
+              )
+            }
+            desc={data[key].desc}
+          />
+        ))
+      ) : (
+        <p style={{ color: "red" }}>Pas de code de triche</p>
+      )}
+    </Layout>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "./index.scss";
+import Simflouz from "./Pages/Simflouz/Simflouz"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import cheats_simflouz from "./Assets/cheats/cheats_simflouz.json"
+import cheats_personnage from "./Assets/cheats/cheats_personnage.json"
+import cheats_construction from "./Assets/cheats/cheats_construction.json"
+
+const router = createBrowserRouter([
+    { path: '/', element: <Simflouz data={cheats_simflouz} /> },
+    { path: '/Douze/', element: <Simflouz data={cheats_simflouz}/> },
+    { path: '/Douze/Simflouz', element: <Simflouz data={cheats_simflouz} /> },
+    { path: '/Douze/Personnage', element: <Simflouz data={cheats_personnage} /> },
+    { path: '/Douze/Construction', element: <Simflouz data={cheats_construction} /> }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
+)
