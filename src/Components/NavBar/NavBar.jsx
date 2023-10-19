e4526891300795a36d8cb22def2f57dd53952118
pro.boooooo <pro.boudjemline@gmail.com>
2023-10-19 18:15:36 +0200

    $

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,25 @@
+import "./NavBar.scss"
+import { Link } from "react-router-dom"
+import logo from "../../Assets/img/logo.png"
+
+export default function NavBar() {
+    return (
+        <nav id="nav-container">
+            <ul id="links-container">
+                <img src={logo} className="logo" alt="logo sims" />
+
+                <Link className="link" to="/Douze/Simflouz" >
+                    Simflouz
+                </Link>
+
+                <Link className="link" to="/Douze/Personnage" >
+                    Personnage
+                </Link>
+
+                <Link className="link" to="/Douze/Construction" >
+                    Construction
+                </Link>
+            </ul>
+        </nav>
+    )
+}
