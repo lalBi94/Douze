da019cdb70ea270292ac7078ea15e78b3a0a5b22
pro.boooooo <pro.boudjemline@gmail.com>
2024-06-16 23:40:37 +0200

    $

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import Simflouz from "./Pages/Simflouz/Simflouz";
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import cheats_simflouz from "./Assets/cheats/cheats_simflouz.json";
 import cheats_personnage from "./Assets/cheats/cheats_personnage.json";
 import cheats_construction from "./Assets/cheats/cheats_construction.json";
 import Like from "./Pages/Like/Like";
 
-const router = createHashRouter([
+const router = createBrowserRouter([
     { path: "/", element: <Simflouz data={cheats_simflouz} /> },
     { path: "/Simflouz", element: <Simflouz data={cheats_simflouz} /> },
     {
