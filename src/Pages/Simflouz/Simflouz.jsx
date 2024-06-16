61cc57b5f8065190e6ee9648caa791b3f3412e4d
pro.boooooo <pro.boudjemline@gmail.com>
2024-06-16 23:31:50 +0200

    $

--- src/Pages/Simflouz/Simflouz.jsx
+++ src/Pages/Simflouz/Simflouz.jsx
@@ -1,58 +1,93 @@
-import CheatMesh from "../../Components/CheatMesh/CheatMesh"
-import Layout from './../../Layout/Layout';
+import CheatMesh from "../../Components/CheatMesh/CheatMesh";
+import Layout from "./../../Layout/Layout";
 
 export default function Simflouz({ data }) {
-  return (
-    <Layout>
-      {Object.keys(data).length > 0 ? (
-        Object.keys(data).map((key) => (
-          <CheatMesh
-            title={data[key].title}
-            code={
-              data[key].args_i && data[key].args_r ? (
-                <div className="code-spec">
-                  <code>{data[key].code}</code>
+    return (
+        <Layout>
+            {data ? (
+                Object.keys(data).length > 0 ? (
+                    Object.keys(data).map((key, v) => (
+                        <CheatMesh
+                            key={key}
+                            meta={key}
+                            title={data[key].title}
+                            code={
+                                data[key].args_i && data[key].args_r ? (
+                                    <div className="code-spec">
+                                        <code>{data[key].code}</code>
 
-                  {data[key].args_i.map((value, index) => (
-                    <input type="text" placeholder={value} key={index} />
-                  ))}
+                                        {data[key].args_i.map(
+                                            (value, index) => (
+                                                <input
+                                                    type="text"
+                                                    placeholder={value}
+                                                    key={index}
+                                                />
+                                            )
+                                        )}
 
-                  <select>
-                    {data[key].args_r.map((value, index) => (
-                      <option id={index} value={data[key].args_r_v[index]}>
-                        {value}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-              ) : data[key].args_i || data[key].args_r ? (
-                <div className="code-spec">
-                  <code>{data[key].code}</code>
+                                        <select>
+                                            {data[key].args_r.map(
+                                                (value, index) => (
+                                                    <option
+                                                        id={index}
+                                                        value={
+                                                            data[key].args_r_v[
+                                                                index
+                                                            ]
+                                                        }
+                                                    >
+                                                        {value}
+                                                    </option>
+                                                )
+                                            )}
+                                        </select>
+                                    </div>
+                                ) : data[key].args_i || data[key].args_r ? (
+                                    <div className="code-spec">
+                                        <code>{data[key].code}</code>
 
-                  {data[key].args_i ? (
-                    data[key].args_i.map((value, index) => (
-                      <input type="text" placeholder={value} key={index} />
+                                        {data[key].args_i ? (
+                                            data[key].args_i.map(
+                                                (value, index) => (
+                                                    <input
+                                                        type="text"
+                                                        placeholder={value}
+                                                        key={index}
+                                                    />
+                                                )
+                                            )
+                                        ) : (
+                                            <select>
+                                                {data[key].args_r.map(
+                                                    (value, index) => (
+                                                        <option
+                                                            value={
+                                                                data[key]
+                                                                    .args_r_v[
+                                                                    index
+                                                                ]
+                                                            }
+                                                            id={index}
+                                                        >
+                                                            {value}
+                                                        </option>
+                                                    )
+                                                )}
+                                            </select>
+                                        )}
+                                    </div>
+                                ) : (
+                                    <code>{data[key].code}</code>
+                                )
+                            }
+                            desc={data[key].desc}
+                        />
                     ))
-                  ) : (
-                    <select>
-                      {data[key].args_r.map((value, index) => (
-                        <option value={data[key].args_r_v[index]} id={index}>
-                          {value}
-                        </option>
-                      ))}
-                    </select>
-                  )}
-                </div>
-              ) : (
-                <code>{data[key].code}</code>
-              )
-            }
-            desc={data[key].desc}
-          />
-        ))
-      ) : (
-        <p style={{ color: "red" }}>Pas de code de triche</p>
-      )}
-    </Layout>
-  );
+                ) : (
+                    <p style={{ color: "red" }}>Pas de code de triche</p>
+                )
+            ) : null}
+        </Layout>
+    );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.scss";
-import Simflouz from "./Pages/Simflouz/Simflouz"
+import Simflouz from "./Pages/Simflouz/Simflouz";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import cheats_simflouz from "./Assets/cheats/cheats_simflouz.json"
-import cheats_personnage from "./Assets/cheats/cheats_personnage.json"
-import cheats_construction from "./Assets/cheats/cheats_construction.json"
+import cheats_simflouz from "./Assets/cheats/cheats_simflouz.json";
+import cheats_personnage from "./Assets/cheats/cheats_personnage.json";
+import cheats_construction from "./Assets/cheats/cheats_construction.json";
+import Like from "./Pages/Like/Like";
 
 const router = createBrowserRouter([
-    { path: '/', element: <Simflouz data={cheats_simflouz} /> },
-    { path: '/Douze/', element: <Simflouz data={cheats_simflouz}/> },
-    { path: '/Douze/Simflouz', element: <Simflouz data={cheats_simflouz} /> },
-    { path: '/Douze/Personnage', element: <Simflouz data={cheats_personnage} /> },
-    { path: '/Douze/Construction', element: <Simflouz data={cheats_construction} /> }
-])
+    { path: "/", element: <Simflouz data={cheats_simflouz} /> },
+    { path: "/Douze/", element: <Simflouz data={cheats_simflouz} /> },
+    { path: "/Douze/Simflouz", element: <Simflouz data={cheats_simflouz} /> },
+    {
+        path: "/Douze/Personnage",
+        element: <Simflouz data={cheats_personnage} />,
+    },
+    {
+        path: "/Douze/Construction",
+        element: <Simflouz data={cheats_construction} />,
+    },
+    { path: "/Douze/Like", element: <Like /> },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <RouterProvider router={router}/>
+        <RouterProvider router={router} />
     </React.StrictMode>
-)
+);
