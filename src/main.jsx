0652cc9957bd9181494002f17a72fd15ffc31259
pro.boooooo <pro.boudjemline@gmail.com>
2024-06-16 23:35:54 +0200

    $

--- src/main.jsx
+++ src/main.jsx
@@ -2,25 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import Simflouz from "./Pages/Simflouz/Simflouz";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import cheats_simflouz from "./Assets/cheats/cheats_simflouz.json";
 import cheats_personnage from "./Assets/cheats/cheats_personnage.json";
 import cheats_construction from "./Assets/cheats/cheats_construction.json";
 import Like from "./Pages/Like/Like";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     { path: "/", element: <Simflouz data={cheats_simflouz} /> },
-    { path: "/Douze/", element: <Simflouz data={cheats_simflouz} /> },
-    { path: "/Douze/Simflouz", element: <Simflouz data={cheats_simflouz} /> },
+    { path: "/Simflouz", element: <Simflouz data={cheats_simflouz} /> },
     {
-        path: "/Douze/Personnage",
+        path: "/Personnage",
         element: <Simflouz data={cheats_personnage} />,
     },
     {
-        path: "/Douze/Construction",
+        path: "/Construction",
         element: <Simflouz data={cheats_construction} />,
     },
-    { path: "/Douze/Like", element: <Like /> },
+    { path: "/Like", element: <Like /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
